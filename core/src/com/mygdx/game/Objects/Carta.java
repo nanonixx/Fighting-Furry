3e1b37b5a89ab4e1fc24efe921b8fef6539bbb66
nanonixx <nhid@elpuig.xeill.net>
2021-04-14 20:43:45 +0200

    empezado screen de seleccion de luchadoR

---
+++ core/src/com/mygdx/game/Objects/Carta.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.Objects;
+
+public class Carta {
+    String nombre;
+    String descripcion;
+    int coste_mana;
+}
--- core/src/com/mygdx/game/SelectionScreen.java
+++ core/src/com/mygdx/game/SelectionScreen.java
@@ -1,15 +1,21 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.game.BaseScreen;
 import com.mygdx.game.MyGdxGame;
+
+import java.awt.*;
 
 public class SelectionScreen extends BaseScreen {
     public SelectionScreen(MyGdxGame game) {
@@ -18,21 +24,42 @@ public class SelectionScreen extends BaseScreen {
 
     @Override
     public void show(){
-    ImageButton.ImageButtonStyle buttonStartStyle = new ImageButton.ImageButtonStyle();
-    buttonStartStyle.up = new TextureRegionDrawable(new TextureRegion(new Texture("start.png")));
-    buttonStartStyle.over = new TextureRegionDrawable(new TextureRegion(new Texture("startpressed.png")));
-    ImageButton buttonStart = new ImageButton(buttonStartStyle);
-        buttonStart.setPosition(30,10);
-    //no cambia de tamaño, no se por qué
-        buttonStart.setSize(24*10, 10*10);
-        buttonStart.addListener(new InputListener(){
-        @Override
-        public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-            setScreen(new GameScreen(game));
-            return true;
-        }
-    });
-        stage.middleCenter.addActor(buttonStart);
+
+        ImageButton.ImageButtonStyle buttonReadyStyle = new ImageButton.ImageButtonStyle();
+        buttonReadyStyle.up = new TextureRegionDrawable(new TextureRegion(new Texture("readyButton.png")));
+        buttonReadyStyle.over = new TextureRegionDrawable(new TextureRegion(new Texture("readyButtonPressed.png")));
+        ImageButton buttonReady = new ImageButton(buttonReadyStyle);
+        buttonReady.setPosition(525,54);
+        buttonReady.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) { setScreen(new GameScreen(game));
+                return true;
+            }
+        });
+        stage.addActor(buttonReady);
+
+
+//        Image header = new Image(new Texture("header.png"));
+//        header.setPosition(0, 720-84);
+//
+//        stage.topCenter.addActor(header);
+
+        Rectangle header = new Rectangle();
+
+
+//        header.setSize(viewport.getScreenWidth(), 85);
+//        header.setPosition(0, viewport.getTopGutterY());
+
+        ShapeRenderer headerRenderer = new ShapeRenderer();
+        headerRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        headerRenderer.setColor(Color.GRAY);
+        headerRenderer.rect(0, 500, viewport.getScreenWidth(), 85);
+        headerRenderer.end();
+
+
+
+
+
     }
 
     @Override
